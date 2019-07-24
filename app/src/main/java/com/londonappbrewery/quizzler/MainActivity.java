9570eb9fb1ec6886334efdee53dd546b6f52a6f3
Max <fma1cr@gmail.com>
2019-07-24 19:23:27 +0500

    Added ProgressBar and Score

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -1,21 +1,22 @@
 package com.londonappbrewery.quizzler;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ProgressBar;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-    // TODO: Declare constants here
-
 
     // 1 - declaring variables
     Button mTrueButton, mFalseButton;
     TextView mQuestionTextView;
     int arrayIndex;
+    int mScore;
+    TextView scoreTextView;
+    ProgressBar progressBar;
 
     // Database of questions
     private TrueFalse[] mQuestionsDB = new TrueFalse[] {
@@ -34,6 +35,9 @@ public class MainActivity extends Activity {
             new TrueFalse(R.string.question_13,true)
     };
 
+    //
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0 / mQuestionsDB.length);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +47,8 @@ public class MainActivity extends Activity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        scoreTextView = (TextView) findViewById(R.id.score);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
         //Setting text to main TextView from array of questions
         mQuestionTextView.setText(mQuestionsDB[arrayIndex].getQuiestionID());
@@ -51,7 +57,7 @@ public class MainActivity extends Activity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
                 updateQuestion();
             }
         });
@@ -59,7 +65,7 @@ public class MainActivity extends Activity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
                 updateQuestion();
             }
         });
@@ -69,5 +75,18 @@ public class MainActivity extends Activity {
     private void updateQuestion(){
         arrayIndex = (arrayIndex + 1) % mQuestionsDB.length;
         mQuestionTextView.setText(mQuestionsDB[arrayIndex].getQuiestionID());
+        progressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+        scoreTextView.setText("Score  " + mScore + "/" + mQuestionsDB.length);
+    }
+
+    //For checking whether user's tap correct
+    private void checkAnswer(boolean usersSelection) {
+        boolean correctAnswer = mQuestionsDB[arrayIndex].isTrueFalse();
+        if (usersSelection == correctAnswer) {
+            Toast.makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            mScore += 1;
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+        }
     }
 }
