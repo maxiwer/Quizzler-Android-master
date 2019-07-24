f5bee291c2a87841b0c08496e443087f9ede8e13
Max <fma1cr@gmail.com>
2019-07-24 13:45:26 +0500

    Edited TextView of questions
    Uncommented kind of database of questions,  added new method of updating question's TextView after tapping

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -13,23 +14,25 @@ public class MainActivity extends Activity {
 
     // 1 - declaring variables
     Button mTrueButton, mFalseButton;
+    TextView mQuestionTextView;
+    int arrayIndex;
 
-    // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    // Database of questions
+    private TrueFalse[] mQuestionsDB = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +42,32 @@ public class MainActivity extends Activity {
         //2 - findViewById() & 3 - casting to required type
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        //Setting text to main TextView from array of questions
+        mQuestionTextView.setText(mQuestionsDB[arrayIndex].getQuiestionID());
 
         //implementing Toast
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+                updateQuestion();
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
+                updateQuestion();
             }
         });
+    }
+
+    //new unique method for updating questions after each tap
+    private void updateQuestion(){
+        arrayIndex = (arrayIndex + 1) % mQuestionsDB.length;
+        mQuestionTextView.setText(mQuestionsDB[arrayIndex].getQuiestionID());
     }
 }
